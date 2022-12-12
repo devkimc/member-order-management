ee160cd9ce93ea4a4b3d607a6f23a9855c9b2777
WooSeok Kim <kws60000@naver.com>
2022-12-12 22:44:04 +0900

    TIL: 스프링 빈 조회

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,22 @@
 package hello.core;
 
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class CoreApplicationTests {
+	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
 	@Test
-	void contextLoads() {
+	@DisplayName("빈 이름으로 조회")
+	void findBeanByName() {
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
+		Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
 	}
 
 }
