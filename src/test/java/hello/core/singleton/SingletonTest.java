295d4cf19842fe0951e1997f79d30fd86c693614
WooSeok Kim <kws60000@naver.com>
2022-12-15 22:57:58 +0900

    TIL: 싱글톤 공유 필드 오류 테스트 추가

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -16,7 +16,7 @@ public class SingletonTest {
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer() {
         AppConfig appConfig = new AppConfig();
-        // 1. 조회: 호출할 때 마다 객체를 생성
+        // 1. 조회: 호출할 때 마다 객체를 생성가
         MemberService memberService1 = appConfig.memberService();
 
         // 2. 조회: 호출할 때 마다 객체를 생성
